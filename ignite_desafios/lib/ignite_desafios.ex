43def2fcb8edbb7d7b8d12359a65142e6715ba51
Felipe.Agricola <felipe.agricola@pbtote.com>
2021-03-10 16:06:45 -0300

    base do projeto

---
+++ ignite_desafios/lib/ignite_desafios.ex
@@ -0,0 +1,18 @@
+defmodule IgniteDesafios do
+  @moduledoc """
+  Documentation for `IgniteDesafios`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IgniteDesafios.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ ignite_desafios/test/ignite_desafios_test.exs
@@ -0,0 +1,8 @@
+defmodule IgniteDesafiosTest do
+  use ExUnit.Case
+  doctest IgniteDesafios
+
+  test "greets the world" do
+    assert IgniteDesafios.hello() == :world
+  end
+end
