ae254c0d8b7ae358d74a05748fbac0da935e8f27
Felipe <fen1499@gmail.com>
2021-03-10 23:01:16 -0300

    wip

--- gen_report/lib/gen_report.ex
+++ gen_report/lib/gen_report.ex
@@ -1,9 +1,12 @@
 defmodule GenReport do
   def call do
-    {:ok, data} = File.read("gen_report.csv")
-    data
-    |> String.split("\r\n")
-    |> Enum.map(fn x -> String.split(x) end)
-    |>List.first()
+    {:ok, data} = File.read("gen_report.csv") #nome, horas, dia, mes, ano
+    lists =
+      data
+      |> String.split("\r\n")
+      |> Enum.map(fn x -> String.split(x, ",") end)
+
+    #Alguma coisa que extrai o valor de lists em uma função recursiva, sei lá
+    #|>List.first()
   end
 end
