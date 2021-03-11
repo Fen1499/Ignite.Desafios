6431d1aacbfc7efe91cd404875f682fc8eb9baaf
Felipe <fen1499@gmail.com>
2021-03-10 22:38:49 -0300

    melhorando a mensagem de retorno

--- list_filter/lib/list_filter.ex
+++ list_filter/lib/list_filter.ex
@@ -2,9 +2,11 @@ defmodule ListFilter do
 
   #Por acaso eu tinha lido sobre enums e maps mais cedo
   def call(list) when is_list(list) do
-    list
-    |> Enum.filter(fn x -> is_number(x) end) #Muito parecido com aquele truque do js, () => function{}
-    |> Enum.count(fn x -> Integer.mod(x,2) == 0 end) #Demorei um pouco pra encontrar o mod, ele não fica no kernel
+    num = list
+      |> Enum.filter(fn x -> is_number(x) end) #Muito parecido com aquele truque do js, () => function{}
+      |> Enum.count(fn x -> Integer.mod(x,2) == 0 end) #Demorei um pouco pra encontrar o mod, ele não fica no kernel
+    "A lista tem um total de #{num} elementos pares"
+
   end
 
 end
