a014657c604389bf621bc33d8557f2e238047e06
Felipe.Agricola <felipe.agricola@pbtote.com>
2021-08-03 15:55:54 -0300

    request ok, falta arrumar o header

--- exapi/lib/exapi_web/controllers/api_controller.ex
+++ exapi/lib/exapi_web/controllers/api_controller.ex
@@ -1,7 +1,9 @@
 defmodule ExapiWeb.ApiController do
   use ExapiWeb, :controller
+  alias Exapi.GitApi.Client
 
   def index(conn, params) do
-
+    %{"username" => username} = params
+    Client.get_user_info(username)
   end
 end
