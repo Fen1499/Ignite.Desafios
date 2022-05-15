ef258ef765efb9238b046ad8d3d8239d73f8e072
Felipe Nascimento Agricola <felipe.agricola@luizalabs.com>
2022-05-15 11:43:01 -0300

    auth ok

--- exapi/lib/exapi_web/auth/pipeline.ex
+++ exapi/lib/exapi_web/auth/pipeline.ex
@@ -1,6 +1,6 @@
 defmodule Exapi.Auth.Pipeline do
   use Guardian.Plug.Pipeline, otp_app: :exapi,
-  module: Exapi.Tokens,
+  module: ExapiWeb.Auth.Guardian,
   error_handler: Exapi.Auth.ErrorHandler
 
   plug Guardian.Plug.VerifyHeader
